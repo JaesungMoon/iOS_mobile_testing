9b0a1438838a1db7c759121bbd4d3d162ce032a0
JaesungMoon <jsdekiru@gmail.com>
2020-03-01 02:29:17 +0900

    first commit for XCTest

---
+++ MyXCTest/MyXCTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyXCTest
+//
+//  Created by JaesungMoon on 2020/03/01.
+//  Copyright © 2020 Jaesung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
